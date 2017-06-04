407d722d75dbbdad5d9da94f8cb7a439575aa72e
yonha0917 <yonha.kim0917@gmail.com>
2017-06-04 11:08:05 +0900

    프로젝트생성
    프로젝트생성

---
+++ PreTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PreTestProject
+//
+//  Created by よんは きむ on 2017. 6. 4..
+//  Copyright © 2017년 yonha kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
